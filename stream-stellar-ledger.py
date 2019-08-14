1a6bc81ea7e711852c2b9f2eab48dd87661b60b1
koltenb <koltenb@tamu.edu>
2019-08-14 16:07:14 -0500

    Update code

--- stream-stellar-ledger.py
+++ stream-stellar-ledger.py
@@ -9,11 +9,11 @@ def data_handler(response):
         print("Failed Transactions:", ledger_data['failed_transaction_count'])
         print("Total Operations:", ledger_data['operation_count'], '\n')
 
-def show_data():
+def get_ledger_data():
     ledger_data = Horizon(horizon_uri='https://horizon.stellar.org')
     ledger_data = ledger_data.ledgers(cursor='now', order='asc', sse=True)
     for ledger in ledger_data:
         data_handler(ledger)
 
 if __name__ == '__main__':
-    show_data()
+    get_ledger_data()
