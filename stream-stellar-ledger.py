92e3947f9bff7f92299b35de7f65775c15777766
koltenb <koltenb@tamu.edu>
2019-08-15 09:58:22 -0500

    Add comments for legibility

--- stream-stellar-ledger.py
+++ stream-stellar-ledger.py
@@ -2,16 +2,21 @@ import json
 from stellar_base.horizon import Horizon
 
 def data_handler(response):
+    # Filter "hello" response  
     if response.__dict__['data'] != '"hello"':
-        ledger_data = json.loads(response.__dict__['data'])
+        # Convert 'data' JSON string to Python object
+        ledger_data = json.loads(response.__dict__['data'])  
         print("Ledger:", ledger_data['sequence'])
         print("Successful Transactions:", ledger_data['successful_transaction_count'])
         print("Failed Transactions:", ledger_data['failed_transaction_count'])
         print("Total Operations:", ledger_data['operation_count'], '\n')
 
 def get_ledger_data():
+    # Create Horizon object 
     ledger_data = Horizon(horizon_uri='https://horizon.stellar.org')
+    # Start streaming from ledgers endpoint 
     ledger_data = ledger_data.ledgers(cursor='now', order='asc', sse=True)
+    # Handle responses 
     for ledger in ledger_data:
         data_handler(ledger)
 
