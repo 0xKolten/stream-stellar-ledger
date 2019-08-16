f6d6777896e5ec6a82f0d439c5353efd776b89ce
koltenb <koltenb@tamu.edu>
2019-08-16 09:08:20 -0500

    Update based on overcat suggestion
    Reflects v 1.3.0+ changes

--- stream-stellar-ledger.py
+++ stream-stellar-ledger.py
@@ -2,14 +2,11 @@ import json
 from stellar_base.horizon import Horizon
 
 def data_handler(response):
-    # Filter "hello" response  
-    if response.__dict__['data'] != '"hello"':
-        # Convert 'data' JSON string to Python object
-        ledger_data = json.loads(response.__dict__['data'])  
-        print("Ledger:", ledger_data['sequence'])
-        print("Successful Transactions:", ledger_data['successful_transaction_count'])
-        print("Failed Transactions:", ledger_data['failed_transaction_count'])
-        print("Total Operations:", ledger_data['operation_count'], '\n')
+    # Print relevant data 
+    print("Ledger:", response['sequence'])
+    print("Successful Transactions:", response['successful_transaction_count'])
+    print("Failed Transactions:", response['failed_transaction_count'])
+    print("Total Operations:", response['operation_count'], '\n')
 
 def get_ledger_data():
     # Create Horizon object 
